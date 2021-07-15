17225966746d7572a498da997b17b143ac6a154c
William Crownover <williamcrownover1@gmail.com>
2021-07-15 15:01:35 -0700

    Developed inquirer promise chain and returns an array of team member objects

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Packages/Modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // Inquirer question arrays
 const nextTeamMember = [
@@ -11,7 +14,7 @@ const nextTeamMember = [
         choices: ["Engineer","Intern","Finished"]
 
     }
-]
+];
 const managerQuestions = [
     {
         type: "input",
@@ -33,7 +36,7 @@ const managerQuestions = [
         message: "What is the team manager's Office Number?",
         name: "officeNumber"
     }
-]
+];
 const engineerQuestions = [
     {
         type: "input",
@@ -55,7 +58,7 @@ const engineerQuestions = [
         message: "What is this engineer's GitHub Username?",
         name: "github"
     }
-]
+];
 const internQuestions = [
     {
         type: "input",
@@ -77,4 +80,61 @@ const internQuestions = [
         message: "What is this intern's School Name?",
         name: "school"
     }
-]
+];
+
+// Stored team members array
+let teamMembers = [];
+
+// Ask the user for info on an engineer team member
+const askforEngineerInfo = () => {
+    return inquirer
+        .prompt(engineerQuestions)
+        .then((engineerAnswers) => {
+            teamMembers.push(new Engineer(engineerAnswers));
+
+            return askforNextTeamMember();
+        });
+}
+
+// Ask the user for info on an intern team member
+const askforInternInfo = () => {
+    return inquirer
+        .prompt(internQuestions)
+        .then((internAnswers) => {
+            teamMembers.push(new Intern(internAnswers));
+
+            return askforNextTeamMember();
+        });
+}
+
+// Ask the user what team member they would like to add or finish process
+const askforNextTeamMember = () => {
+    return inquirer
+        .prompt(nextTeamMember)
+        .then((nextTeamMemberAnswer) => {
+            switch(nextTeamMemberAnswer.teamMember) {
+                case "Engineer":
+                    return askforEngineerInfo();
+                case "Intern":
+                    return askforInternInfo();
+                default:
+                    return;
+            }
+        });
+}
+
+// Initialize the question process
+const init = () => {
+    inquirer
+        .prompt(managerQuestions)
+        .then((managerAnswers) => {
+            teamMembers.push(new Manager(managerAnswers));
+
+            return askforNextTeamMember();
+        })
+        .then(() => {
+            console.log(teamMembers);
+        })
+};
+
+init();
