6a40082f51644302be76d3e7c3ee8afd9a3e8620
William Crownover <williamcrownover1@gmail.com>
2021-07-15 14:27:10 -0700

    Listed inquirer questions as array sets for each team member type

--- index.js
+++ index.js
@@ -3,11 +3,78 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Inquirer question arrays
+const nextTeamMember = [
+    {
+        type: "list",
+        message: "Would you like to add another team member?",
+        name: "teamMember",
+        choices: ["Engineer","Intern","Finished"]
+
+    }
+]
 const managerQuestions = [
     {
         type: "input",
-        message: "What is the team manager's name?",
+        message: "What is the team manager's Name?",
         name: "name"
     },
-
+    {
+        type: "input",
+        message: "What is the team manager's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's Email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's Office Number?",
+        name: "officeNumber"
+    }
+]
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "What is this engineer's Name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is this engineer's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is this engineer's Email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is this engineer's GitHub Username?",
+        name: "github"
+    }
+]
+const internQuestions = [
+    {
+        type: "input",
+        message: "What is this intern's Name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is this intern's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is this intern's Email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is this intern's School Name?",
+        name: "school"
+    }
 ]
